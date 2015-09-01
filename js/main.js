484ff772c59a8bebe843425854deb853965ea409
Yurii Shevchuk <mail@itdxer.com>
2015-09-01 12:14:17 +0300

    Update website.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var state;
+function fillBackground() {
+    canvasBlocks = document.getElementById('canvas').getElementsByTagName('canvas');
+
+    if (canvasBlocks.length > 0) {
+        canvasBlocks[0].remove();
+    }
+    var pattern = Trianglify({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        x_colors: 'RdBu',
+        cell_size: 50
+    });
+    document.getElementById('canvas').appendChild(pattern.canvas());
+}
+function onWindowResize() {
+    clearTimeout(state);
+    state = setTimeout(fillBackground, 100);
+}
+
+window.addEventListener('resize', fillBackground);
+fillBackground();
